be53c8f472fa829f4a54bbc1c73c80fad47f1108
EvAlexVi <AlisiAlexEv@outlook.com>
2013-08-30 21:52:52 +0400

    Main, start logic

--- src/com/Main/Main.java
+++ src/com/Main/Main.java
@@ -1,21 +1,21 @@
-package Main;
+package com.Main;
 
 import com.Logic.Game.Space;
 
 import javax.swing.*;
 
 public class Main extends JFrame {
-    public Main (){
+    public void Ma (){
         add(new Space());
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
+        setSize(600, 400);
         setLocationRelativeTo(null);
-        setTitle("R - Type");
+        setTitle("SpaceBattle");
         setResizable(false);
         setVisible(true);
     }
     public static void main(String[] args) {
-        new Space();
+        Main main = new Main();
+        main.Ma();
     }
 }
