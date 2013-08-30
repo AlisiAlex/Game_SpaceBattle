ce0f64cee44f951cf43d4250e0e8ac580af05a0b
EvAlexVi <AlisiAlexEv@outlook.com>
2013-08-30 21:20:38 +0400

    Move Shatel in the Space

--- src/com/Logic/Game/Flight.java
+++ src/com/Logic/Game/Flight.java
@@ -1,4 +1,59 @@
 package com.Logic.Game;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class Flight {
+    private String craft = "Shatl.png";
+
+    private int dx;
+    private int dy;
+    private int x;
+    private int y;
+    private Image image;
+
+    public Flight(){
+        ImageIcon imageIcon = new ImageIcon(this.getClass().getResource(craft));
+        image = imageIcon.getImage();
+        x = 40;
+        y = 60;
+    }
+
+    public void move(){
+        x += dx;
+        y += dy;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
 }
