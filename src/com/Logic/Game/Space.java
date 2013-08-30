5d717bed9eb1e3c148312ef9be475ac45cf59e9b
EvAlexVi <AlisiAlexEv@outlook.com>
2013-08-30 21:52:39 +0400

    Main, start logic

--- src/com/Logic/Game/Space.java
+++ src/com/Logic/Game/Space.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class Space extends JPanel implements ActionListener {
 
@@ -31,11 +32,28 @@ public class Space extends JPanel implements ActionListener {
         Graphics2D g2d = (Graphics2D)g;
         g2d.drawImage(craft.getImage(), craft.getX(), craft.getY(), this);
 
+        ArrayList ms = craft.getMissiles();
+
+        for (int i = 0; i < ms.size(); i++ ) {
+            Missile m = (Missile) ms.get(i);
+            g2d.drawImage(m.getImage(), m.getX(), m.getY(), this);
+        }
+
         Toolkit.getDefaultToolkit().sync();
         g.dispose();
     }
 
+
     public void actionPerformed(ActionEvent e) {
+        ArrayList ms = craft.getMissiles();
+
+        for (int i = 0; i < ms.size(); i++) {
+            Missile m = (Missile) ms.get(i);
+            if (m.isVisible())
+                m.move();
+            else ms.remove(i);
+        }
+
         craft.move();
         repaint();
     }
